d7ffa5b5cfac0cf714382a74aa937ff994f9efd3
andrew <aconnelly@sourceallies.com>
2017-03-03 16:56:35 -0600

    filling out results set mapper for family member, added contact info to family

--- services/src/main/java/org/auditioner/services/family/Family.java
+++ services/src/main/java/org/auditioner/services/family/Family.java
@@ -3,6 +3,28 @@ package org.auditioner.services.family;
 public class Family {
     private String location;
     private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String preferredContactMethod;
+
+    public String getPreferredContactMethod() { return preferredContactMethod; }
+
+    public void setPreferredContactMethod(String preferredContactMethod) {
+        this.preferredContactMethod = preferredContactMethod;
+    }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getAddress() { return address; }
+
+    public void setAddress(String address) { this.address = address; }
 
     public String getName() {
         return name;
