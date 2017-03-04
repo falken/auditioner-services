eb7fb8a5916384910213ca9558955439aeddf9c6
Snehansh Akhaury <Snehansh Akhaury>
2017-03-04 13:59:00 -0600

    adding family member ui

--- web/app/families/family-member/route.js
+++ web/app/families/family-member/route.js
@@ -1,17 +1,18 @@
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	familyService: Ember.inject.service('family-service'),
-	productionService: Ember.inject.service('production-service'),
-
-	model(param){
-		console.log(param);
-
-		const familyService = this.get('familyService');
-		const productionService = this.get('productionService');
-
-		return {
-			productions: productionService.searchProductions()
-		};
-	}
+  familyService: Ember.inject.service('family-service'),
+  productionService: Ember.inject.service('production-service'),
+  model(param){
+    const familyService = this.get('familyService');
+    const productionService = this.get('productionService');
+		// console.log(familyService.loadFamilyMemberById(param.family_id,param.family_member_id));
+		// familyService.loadFamilyMemberById(param.family_id,param.family_member_id)
+    return {
+      family: familyService.loadFamilyById(param.family_id),
+			familyMember:familyService.loadFamilyMemberById(param.family_id,param.family_member_id),
+      productions: productionService.searchProductions()
+    };
+  }
 });
