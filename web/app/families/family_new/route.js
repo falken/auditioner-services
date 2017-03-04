5e452947f5154ec2188f1268c93aa2f8545945d8
Steven Nelson <snelson@iowaballetacademy.com>
2017-03-04 16:39:47 -0600

    Cleanup

--- web/app/families/family_new/route.js
+++ web/app/families/family_new/route.js
@@ -6,10 +6,11 @@ export default Ember.Route.extend({
   productionService: Ember.inject.service('production-service'),
   model(){
     const familyService = this.get('familyService');
-    const model =  {
-      family:familyService.createFamily(),
+    const family = familyService.createFamily();
+    family.set('preferredContactMethod','Email');
+    return {
+      family:family,
     };
-    return model;
   },
   setupController: function (controller,model) {
     this._super(controller,model);
