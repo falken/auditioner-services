6e0f122ddb9dab3417f166c4d77919228e5d37f0
Peter Hanson <peter@upandrunningsoftware.com>
2017-03-05 23:45:13 -0600

    updated the fields for family in the family template and in the model, so updates and review of family details works

--- web/app/models/family.js
+++ web/app/models/family.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Object.extend({
   path: null,
   name: null,
+  /*
   email: null,
   phone: null,
   address01: null,
@@ -11,19 +12,20 @@ export default Ember.Object.extend({
   state: null,
   zipCode: null,
   preferredContactMethod: null,
+  */
     loadFromData: function (client, data) {
     this.set('client', client);
 
     this.set('id', data.location.substring(data.location.lastIndexOf("/") + 1));
     this.set('location', data.location);
     this.set('name', data.name);
-    this.set('contactEmail', data.email);
-    this.set('contactPhone', data.phone);
-    this.set('mailingAddressLine1', data.address01);
-    this.set('mailingAddressLine2', data.address02);
-    this.set('mailingAddressCity', data.city);
-    this.set('mailingAddressState', data.state);
-    this.set('mailingAddressZip', data.zipCode);
+    this.set('email', data.email);
+    this.set('phone', data.phone);
+    this.set('address01', data.address01);
+    this.set('address02', data.address02);
+    this.set('city', data.city);
+    this.set('state', data.state);
+    this.set('zipCode', data.zipCode);
     this.set('preferredContactMethod', data.preferredContactMethod);
 
 
@@ -31,13 +33,13 @@ export default Ember.Object.extend({
   toJson: function () {
     return JSON.stringify({
       name: this.get('name'),
-      email: this.get('contactEmail'),
-      phone: this.get('contactPhone'),
-      address01: this.get('mailingAddressLine1'),
-      address02: this.get('mailingAddressLine2'),
-      city: this.get('mailingAddressCity'),
-      state: this.get('mailingAddressState'),
-      zipCode: this.get('mailingAddressZip'),
+      email: this.get('email'),
+      phone: this.get('phone'),
+      address01: this.get('address01'),
+      address02: this.get('address02'),
+      city: this.get('city'),
+      state: this.get('state'),
+      zipCode: this.get('zipCode'),
       preferredContactMethod: this.get('preferredContactMethod'),
 
     });
