a969ea7556d5fdca3b853d1bcd36c9aefac5bc26
Steven Nelson <snelson@iowaballetacademy.comy>
2017-03-08 19:32:38 -0600

    Fix phone number formatting

--- web/app/models/family.js
+++ web/app/models/family.js
@@ -3,24 +3,20 @@ import Ember from 'ember';
 export default Ember.Object.extend({
   path: null,
   name: null,
-  /*
-  email: null,
-  phone: null,
-  address01: null,
-  address02: null,
-  city: null,
-  state: null,
-  zipCode: null,
-  preferredContactMethod: null,
-  */
-    loadFromData: function (client, data) {
+  loadFromData: function (client, data) {
     this.set('client', client);
 
     this.set('id', data.location.substring(data.location.lastIndexOf("/") + 1));
     this.set('location', data.location);
     this.set('name', data.name);
     this.set('email', data.email);
-    this.set('phone', data.phone);
+
+    let phone = data.phone;
+    if (phone && phone.length === 10) {
+      phone = "(" + phone.substr(0, 3) + ") " + phone.substr(3, 3) + "-" + phone.substr(6, 4);
+    }
+
+    this.set('phone', phone);
     this.set('address01', data.address01);
     this.set('address02', data.address02);
     this.set('city', data.city);
@@ -31,10 +27,18 @@ export default Ember.Object.extend({
 
   },
   toJson: function () {
+
+    let phone = this.get('phone');
+    console.log(phone);
+    if (phone) {
+      phone = phone.replace(/[^0-9.]/g, "").substr(0, 10);
+    }
+    console.log(phone);
+
     return JSON.stringify({
       name: this.get('name'),
       email: this.get('email'),
-      phone: this.get('phone'),
+      phone: phone,
       address01: this.get('address01'),
       address02: this.get('address02'),
       city: this.get('city'),
