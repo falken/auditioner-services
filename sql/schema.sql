965d38a46818302ae1bd5e3afa66945e314ec94f
Steven Nelson <snelson@iowaballetacademy.comy>
2017-03-08 19:01:33 -0600

    Add toggle button so Nav is available on mobile devices

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,27 +1,8 @@
 
 CREATE DATABASE auditioner;
-USER auditioner;
+USE auditioner;
 
 CREATE USER 'auditioner'@'localhost' IDENTIFIED BY 'Password1!';
 
-GRANT ALL PRIVILEGES ON *.* TO 'auditioner'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'auditioner'@'localhost';
 
-CREATE TABLE `Family` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(48) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `Production` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(48) NOT NULL,
-  `auditionDate` date NOT NULL,
-  `season` varchar(48) NOT NULL
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `ProductionMember` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT
-    `production`
-)
